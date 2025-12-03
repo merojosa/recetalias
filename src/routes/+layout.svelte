e7b4339579827b3d3826ad9730917a6f608092a8
Jose <jose.andresmr@hotmail.com>
2025-12-03 16:05:39 -0600

    Content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,4 +9,16 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<header class="py-4 flex justify-center">Recetalias header</header>
+<main class="max-w-[650px] mx-[20vw] flex-1">
+	{@render children()}
+</main>
+<footer class="py-4 flex justify-center">Recetalias footer</footer>
+
+<style>
+	:global(body) {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+</style>
--- src/routes/receta/[id]/+page.svelte
+++ src/routes/receta/[id]/+page.svelte
@@ -2,4 +2,36 @@
 	let { data } = $props();
 </script>
 
-<h1>{data.recipe.title}</h1>
+<svelte:head>
+	<title>{data.recipe.title} - Recetalias</title>
+</svelte:head>
+
+<article>
+	<h1>{data.recipe.title}</h1>
+
+	{#if data.recipe.image}
+		<img src={data.recipe.image.url} alt={data.recipe.image.alt} />
+	{/if}
+
+	<p>
+		<time datetime={data.recipe.date}>{new Date(data.recipe.date).toLocaleDateString()}</time>
+	</p>
+
+	<section>
+		<h2>Ingredients</h2>
+		<ul>
+			{#each data.recipe.ingredients as ingredient (ingredient)}
+				<li>{ingredient}</li>
+			{/each}
+		</ul>
+	</section>
+
+	<section>
+		<h2>Instructions</h2>
+		<ol>
+			{#each data.recipe.instructions as instruction (instruction)}
+				<li>{instruction}</li>
+			{/each}
+		</ol>
+	</section>
+</article>
