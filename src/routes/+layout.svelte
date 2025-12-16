565ade30e98dd69fa04b4b657825cfee8d020d78
Jose <jose.andresmr@hotmail.com>
2025-12-16 01:00:44 -0600

    Improve design

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,15 +11,17 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<header class="py-6 flex justify-center items-center gap-3 font-bold text-4xl">
-	<a href={resolve('/')} class="flex items-center gap-3">
-		<img src={logo} alt="Logo de Recetalias" class="h-28" />
-	</a>
+<header class="p-4 flex justify-center items-center gap-3 font-bold text-4xl">
+	<nav class="w-full flex justify-center p-4 border-4 border-black">
+		<a href={resolve('/')} class="flex items-center gap-3">
+			<img src={logo} alt="Logo de Recetalias" class="h-28" />
+		</a>
+	</nav>
 </header>
 <main class="min-w-[650px] max-w-[650px] mx-[5vw] md:mx-auto flex-1 py-6">
 	{@render children()}
 </main>
-<footer class="py-4 flex justify-center flex-col items-center">
+<footer class="bg-black text-white py-4 flex justify-center flex-col items-center">
 	<p>Recetas por Natalia Díaz</p>
 	<p>Desarrollo por Jose Andrés Mejías</p>
 </footer>
